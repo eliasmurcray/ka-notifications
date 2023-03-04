44c80d2166fe946fadb9f82b2b8b8286b5eaa4be
eliasmurcray <eliasmurcray@gmail.com>
2023-03-04 14:22:11 -0800

    Monkey patch underway

--- src/ts/reply.ts
+++ src/ts/reply.ts
@@ -1,6 +1,50 @@
-let oldParse = JSON.parse;
-JSON.parse = function() {
-  let data = oldParse.apply(this, arguments);
-  console.log(data);
-  return data;
+import QUERIES from "../graphql-queries.json";
+
+const originalFetch = fetch;
+window.fetch = function(request: Request) {
+  return new Promise((resolve) => {
+    let url = request.url;
+    console.log(url);
+    if (url?.startsWith("https://www.khanacademy.org/api/internal/graphql/getFeedbackRepliesPage")) {
+      request.blob()
+      .then((blob) => {
+        let reader = new FileReader();
+        reader.onloadend = () => {
+          let result = reader.result as string;
+          let json = atob(result.split(',')[1]);
+          let obj = JSON.parse(json);
+          let { postKey } = obj.variables;
+          let fkey = getCookie("fkey");
+          console.log("\n\n---\n\n");
+          resolve(graphQLFetch("getFeedbackRepliesPage", fkey, { postKey, limit: 10 }));
+        };
+        reader.readAsDataURL(blob);
+      });
+    }
+  
+    resolve(originalFetch(request));
+  });
 };
+
+
+function graphQLFetch(query: string, fkey: string, variables = {}): Promise<Response> {
+  return originalFetch("https://www.khanacademy.org/api/internal/graphql/" + query + "?/math/", {
+    method: "POST",
+    headers: {
+      "X-KA-fkey": fkey,
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      operationName: query,
+      query: QUERIES[query],
+      variables
+    }),
+    credentials: "same-origin"
+  });
+}
+
+function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
