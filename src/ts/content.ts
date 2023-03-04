c0e02d27dc43f3d64c34928ad62c42435f1255e5
eliasmurcray <eliasmurcray@gmail.com>
2023-03-03 20:25:25 -0800

    Working on reply loading patch

--- src/ts/content.ts
+++ src/ts/content.ts
@@ -1,12 +1,15 @@
 const { search, pathname } = window.location;
 const split = pathname.split("/");
-const twoBytes = /^\d{16}$/;
-const isProject = (split[1] === "computer-programming" || split[1] === "cs") && twoBytes.test(split[2]);
+const isProject = (split[1] === "computer-programming" || split[1] === "cs") && /^\d{16}$/.test(split[3]);
 const params = new URLSearchParams(search);
-
 
 if(isProject) main();
 
 function main() {
-  
+  console.log("Hello from KA Notifications Extension!");
+  let oldParse = JSON.parse;
+  JSON.parse = function(args) {
+    console.log(args);
+    oldParse(args);
+  };
 }
