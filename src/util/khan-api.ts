c37322da36e1bcc914366c287080c9dc5996e82c
eliasmurcray <eliasmurcray@gmail.com>
2023-12-31 03:22:36 -0800

    Complete background notification refresher

--- src/util/khan-api.ts
+++ src/util/khan-api.ts
@@ -1,13 +1,18 @@
+import { getLatestMutation, getLatestQuery } from "@bhavjit/khan-api";
 import { KhanAPIVariables } from "../@types/extension-types";
 import { StringMap } from "../@types/common-types";
 import * as QUERIES_JSON from "../json/khan-api-queries.json";
 
 const QUERIES: StringMap = { ...QUERIES_JSON };
+delete QUERIES.default;
 
-export async function khanApiFetch(queryName: string, authToken: string, variables: KhanAPIVariables = {}): Promise<Response> {
+export async function khanApiFetch(
+	queryName: string,
+	authToken: string,
+	variables: KhanAPIVariables = {},
+): Promise<Response> {
 	const requestURL = `https://www.khanacademy.org/api/internal/graphql/${queryName}?/fastly/`;
 	const query = QUERIES[queryName];
-	const stringifiedVariables = JSON.stringify(variables);
 
 	const requestInit: RequestInit = {
 		method: "POST",
@@ -15,24 +20,36 @@ export async function khanApiFetch(queryName: string, authToken: string, variabl
 			"X-KA-fkey": authToken,
 			"Content-Type": "application/json",
 		},
-		body: `{"operationName":"${queryName}","query":"${query}","variables":${stringifiedVariables}}`,
+		body: JSON.stringify({
+			operationName: queryName,
+			query,
+			variables,
+		}),
 		credentials: "same-origin",
 	};
 
 	const response = await fetch(requestURL, requestInit);
 	if (response.ok) return response;
 
-	if (response.status === 400) {
-		console.warn(`The query for operation "${queryName}" is no longer in the safelist. Attempting to fetch the latest version from the safelist...`);
+	if (response.status === 403) {
+		console.warn(
+			`The query for operation "${queryName}" is no longer in the safelist. Attempting to fetch the latest version from the safelist...`,
+		);
 
-		const latestQuery = query.startsWith("mutation") ? await getLatestMutation(queryName) : await getLatestQuery(queryName);
+		const latestQuery = query.startsWith("mutation")
+			? await getLatestMutation(queryName)
+			: await getLatestQuery(queryName);
 
 		if (!latestQuery) {
 			throw new Error(`The query for operation "${queryName}" was not found in the safelist`);
 		}
 
-		requestInit.body = `{"operationName":"${queryName}","query":"${query}","variables":${stringifiedVariables}}`;
-		
+		requestInit.body = JSON.stringify({
+			operationName: queryName,
+			query: latestQuery,
+			variables,
+		});
+
 		const updatedResponse = await fetch(requestURL, requestInit);
 		if (updatedResponse.ok) {
 			QUERIES[queryName] = latestQuery;
@@ -43,16 +60,16 @@ export async function khanApiFetch(queryName: string, authToken: string, variabl
 	throw new Error(`"${queryName}" responded with status of ${response.status}`);
 }
 
-export function getAuthToken(): Promise<string | undefined> {
-  return new Promise<string>((resolve) => {
-    chrome.cookies.get(
-      {
-        url: "https://www.khanacademy.org",
-        name: "fkey",
-      },
-      (cookie) => {
-        resolve(cookie?.value);
-      },
-    );
-  });
+export function getAuthToken(): Promise<string> {
+	return new Promise<string>((resolve) => {
+		chrome.cookies.get(
+			{
+				url: "https://www.khanacademy.org",
+				name: "fkey",
+			},
+			(cookie) => {
+				resolve(cookie?.value ?? "");
+			},
+		);
+	});
 }
