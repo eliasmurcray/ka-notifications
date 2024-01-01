a603f578dbd3387d5706249a300cb39839d0a4cc
eliasmurcray <eliasmurcray@gmail.com>
2023-12-31 18:08:34 -0800

    Implement page switching and import popup CSS

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -1,6 +1,13 @@
-console.log("Popup opened");
-chrome.storage.local
-	.get(["prefetch_data", "prefetch_cursor"])
-	.then(({ prefetch_data, prefetch_cursor }) => {
-		console.log(prefetch_data, prefetch_cursor);
-	});
+import "../css/popup.css";
+
+const settingsSection = document.getElementById("settings-section") as HTMLDivElement;
+const notificationsSection = document.getElementById("notifications-section") as HTMLDivElement;
+const pageButton = document.getElementById("page-button") as HTMLButtonElement;
+const settingsIcon = document.getElementById("settings-icon") as HTMLElement & SVGElement;
+const backIcon = document.getElementById("back-icon") as HTMLElement & SVGElement;
+pageButton.onclick = () => {
+	notificationsSection.classList.toggle("hidden");
+	settingsSection.classList.toggle("hidden");
+	settingsIcon.classList.toggle("hidden");
+	backIcon.classList.toggle("hidden");
+};
