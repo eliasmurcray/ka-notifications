91fe7d2b1424777e093ef420b5b888abcf002507
eliasmurcray <eliasmurcray@gmail.com>
2024-01-01 15:50:52 -0800

    Add onscroll functionality

--- src/ts/popup.ts
+++ src/ts/popup.ts
@@ -1,3 +1,4 @@
+import { getAuthToken, khanApiFetch } from "../util/khan-api";
 import { createNotificationString, addReplyButtonEventListeners } from "../util/notification-utils";
 import "../css/popup.css";
 
@@ -17,9 +18,10 @@ pageButton.onclick = () => {
 // Notifications from local storage
 const notificationsContainer = document.getElementById("notifications-container") as HTMLDivElement;
 const loadingSpinner = document.getElementById("loading-spinner-container") as HTMLDivElement;
-let __global_cursor = "";
+let __global_cursor__ = "";
+let __loading_notifications__ = false;
 chrome.storage.local.get(["prefetchCursor", "prefetchData"], ({ prefetchCursor, prefetchData }) => {
-	__global_cursor = prefetchCursor;
+	__global_cursor__ = prefetchCursor ?? "";
 	switch (prefetchData) {
 		case "$logged_out":
 			notificationsContainer.innerHTML =
@@ -43,4 +45,64 @@ chrome.storage.local.get(["prefetchCursor", "prefetchData"], ({ prefetchCursor, 
 			notificationsContainer.innerHTML = prefetchData.map(createNotificationString).join("");
 			addReplyButtonEventListeners();
 	}
+
+	notificationsSection.onscroll = handleScroll;
 });
+
+function handleScroll(): void {
+	if (
+		!__loading_notifications__ &&
+		Math.abs(
+			notificationsSection.scrollHeight -
+				notificationsSection.scrollTop -
+				notificationsSection.clientHeight,
+		) < 77
+	) {
+		__loading_notifications__ = true;
+		loadNotifications();
+	}
+}
+
+async function loadNotifications(): Promise<void> {
+	const token = await getAuthToken();
+	if (!token) {
+		notificationsContainer.innerHTML =
+			'<li class="notification new"><div class="notification-header"><img class="notification-author-avatar" src="32.png"><h3 class="notification-author-nickname">KA Notifications</h3></div><div class="notification-content">You are logged out. Please <a class="hyperlink" href="https://khanacademy.org/login" target="_blank">log in to Khan Academy</a> to use this extension.</div></li>';
+		notificationsSection.onscroll = null;
+		loadingSpinner.remove();
+		return;
+	}
+	try {
+		const notificationsResponse = await khanApiFetch("getNotificationsForUser", token, {
+			after: __global_cursor__ || "",
+		});
+		const notificationsJSON = await notificationsResponse.json();
+		const notifications = notificationsJSON?.data?.user?.notifications;
+		if (!notifications) {
+			notificationsContainer.innerHTML =
+				'<li class="notification new"><div class="notification-header"><img class="notification-author-avatar" src="32.png"><h3 class="notification-author-nickname">KA Notifications</h3></div><div class="notification-content">You have no notifications.</div></li>';
+			notificationsSection.onscroll = null;
+			loadingSpinner.remove();
+			return;
+		}
+
+		notificationsContainer.innerHTML +=
+			notifications.notifications.map(createNotificationString);
+
+		if (notifications.pageInfo?.nextCursor === null) {
+			notificationsSection.onscroll = null;
+			loadingSpinner.remove();
+		} else {
+			__global_cursor__ = notifications.pageInfo.nextCursor;
+			__loading_notifications__ = false;
+		}
+	} catch (err) {
+		if (err instanceof Error && err.message === "Failed to fetch") {
+			console.warn(
+				"Possible network disconnect detected, please check your internet connection.",
+			);
+		} else {
+			console.error(err);
+		}
+	}
+}
