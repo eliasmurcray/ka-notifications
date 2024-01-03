94a92b667bdc95fa9846727195d0d6067f5eb070
eliasmurcray <eliasmurcray@gmail.com>
2024-01-02 21:21:20 -0800

    Add 100 comment loader, fix default feedback tab

--- src/ts/content.ts
+++ src/ts/content.ts
@@ -39,32 +39,97 @@ if (
 		}
 
 		button.click();
+		window.scrollTo(0, 0);
+
+		// Auto sort comments
+		chrome.storage.local.get("defaultCommentSort", ({ defaultCommentSort }) => {
+			if (!defaultCommentSort) return;
+			iter = 10000; // Retry amount
+			const setCommentSort = () => {
+				if (!--iter) return;
+				const button = document.getElementById("sortBy");
+				if (!button || !(button instanceof HTMLButtonElement)) {
+					window.requestAnimationFrame(setCommentSort);
+					return;
+				}
+				button.click();
+
+				const sortButtons = document.querySelectorAll<HTMLButtonElement>(
+					"div[data-test-id='dropdown-core-container'] button",
+				);
+				sortButtons.forEach((btn) => {
+					if (btn.innerHTML.includes(defaultCommentSort)) {
+						btn.click();
+					}
+				});
+				window.scrollTo(0, 0);
+				button.blur();
+			};
+			setCommentSort();
+		});
 	};
 
 	window.requestAnimationFrame(openFeedbackTab);
 
-	// Auto sort comments
-	chrome.storage.local.get("defaultCommentSort", ({ defaultCommentSort }) => {
-		if (!defaultCommentSort) return;
-		iter = 10000; // Retry amount
-		const setCommentSort = () => {
-			if (!--iter) return;
-			const button = document.getElementById("sortBy");
-			if (!button || !(button instanceof HTMLButtonElement)) {
-				window.requestAnimationFrame(setCommentSort);
-				return;
-			}
-			button.click();
+	// Fetch 100 comments at a time instead of 10
+	const oldFetch = fetch;
 
-			const sortButtons = document.querySelectorAll<HTMLButtonElement>(
-				"div[data-test-id='dropdown-core-container'] button",
-			);
-			sortButtons.forEach((btn) => {
-				if (btn.innerHTML.includes(defaultCommentSort)) {
-					btn.click();
-				}
-			});
-		};
-		setCommentSort();
-	});
+	window.fetch = async function (
+		input: RequestInfo | URL,
+		init?: RequestInit,
+	): Promise<Response> {
+		if (input instanceof URL || input instanceof String) {
+			return oldFetch(input, init);
+		}
+
+		if (!(input instanceof Request)) {
+			return new Response(null);
+		}
+
+		if (
+			!input.url?.startsWith(
+				"https://www.khanacademy.org/api/internal/graphql/getFeedbackRepliesPage",
+			)
+		) {
+			try {
+				return await oldFetch(input, init);
+			} catch (error) {
+				return new Response(null, {
+					status: 500,
+					statusText: "Internal Server Error",
+				});
+			}
+		}
+
+		return new Promise(async (resolve) => {
+			const blob = await input.blob();
+
+			const reader = new FileReader();
+			reader.onload = async () => {
+				const contents = reader.result as string;
+				const graphQLBody = JSON.parse(atob(contents.split(",")[1]));
+
+				// Increase comment load amount to 100
+				graphQLBody.variables.limit = 100;
+
+				const requestInit: RequestInit = {
+					body: JSON.stringify(graphQLBody),
+					cache: input?.cache,
+					credentials: input?.credentials,
+					headers: input?.headers,
+					integrity: input?.integrity,
+					keepalive: input?.keepalive,
+					method: input?.method,
+					mode: input?.mode,
+					redirect: input?.redirect,
+					referrer: input?.referrer,
+					referrerPolicy: input?.referrerPolicy,
+					signal: input?.signal,
+				};
+				const updatedRequest = new Request(input.url, requestInit);
+				resolve(oldFetch(updatedRequest));
+			};
+			reader.readAsDataURL(blob);
+		});
+	};
 }
